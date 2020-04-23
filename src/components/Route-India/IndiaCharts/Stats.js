67881c2631a8db0b5d2618e2f4bd5e53888a1113
saket sn.3 <saketns@gmail.com>
2020-04-23 21:46:08 +0530

    added icons

--- src/components/Route-India/IndiaCharts/Stats.js
+++ src/components/Route-India/IndiaCharts/Stats.js
@@ -11,7 +11,7 @@ class Stats extends Component{
                <div id="statswindow" class="container shadow p-3 mt-4 mb-3" style={{width:"90%"}}>
                 <h5 class="text-muted text-center">FLASH INSIGHTS <i class="fas fa-chart-bar"></i></h5>
                 <div className="text-center mb-2">
-                    <small className="text-muted text-center">{new Date().toUTCString().slice(5,11)}, {this.props.insights.latestUpdate.timeStamp.slice(11,16)} IST</small>
+                    <small className="text-muted text-center"><i class="far fa-bell mr-1"></i>{new Date().toUTCString().slice(5,11)}, {this.props.insights.latestUpdate.timeStamp.slice(11,16)} IST</small>
                 </div>
                 <div id="stats border-bottom">
                         <div className="text-center">
