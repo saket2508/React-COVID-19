f24c3e022bbd08f1a13ab2fa65a536923f67512b
Saket S <52590360+saket2508@users.noreply.github.com>
2020-07-30 10:39:46 +0530

    Update AppHeading.js

--- src/components/AppHeading.js
+++ src/components/AppHeading.js
@@ -26,7 +26,7 @@ class AppHeading extends Component{
         <div className='container shadow-sm pt-1 pl-3 pr-3 pb-1 mb-4 bg-white rounded'>
         <div className='mt-3'>
             <p class='lead'>
-                See live statistics of the ongoing COVID-19 pandemic that includes the current count of cases, deaths, recovered and active infections in all affected countries. You can search any country to see its figures from the search bar and the table for more insights.
+                See live statistics of the ongoing COVID-19 pandemic that include the current count of cases, deaths, recovered and active infections in all affected countries. You can search any country to see its figures from the search bar and the table for more insights.
             </p>
         </div>
 
