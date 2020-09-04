cf52e3c1053f65725624d109e551a71bf1354073
saket sn.3 <saketns@gmail.com>
2020-09-04 09:42:06 +0530

    edited README.md

--- src/components/AppHeading.js
+++ src/components/AppHeading.js
@@ -12,7 +12,7 @@ export default function AppHeading({ data }){
         <div className='container-lg shadow-sm pt-1 pl-3 pr-3 pb-1 mb-4 bg-white rounded'>
             <div className='mt-3'>
                 <p style={{fontSize:20, fontWeight:'300'}} className="mb-2">
-                    See live statistics of the ongoing COVID-19 pandemic that include the current count of cases, deaths, recovered and active infections in all affected countries. You can search for any country from the search bar to its see figures and charts and the table below it for more insights.
+                    See live statistics of the ongoing COVID-19 pandemic that include the current count of cases, fatalities, recovered patients and active infections in all affected countries. You can search for any country from the search bar to see its charts and figures, and the table below it for more info.
                 </p>
             </div>
             <div style={{flex:1,flexDirection:'row'}} className='mt-3'>
