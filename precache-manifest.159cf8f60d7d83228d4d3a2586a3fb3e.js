7d6a5163c99d2fb8c11440eb52e2ccbe738e7127
saket sn.3 <saketns@gmail.com>
2020-09-21 09:03:57 +0530

    Updates

---
+++ precache-manifest.159cf8f60d7d83228d4d3a2586a3fb3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab14b09f57886412f8025176b28a534e",
+    "url": "/Coronavirus-React/index.html"
+  },
+  {
+    "revision": "eae77b9b32e0981097c4",
+    "url": "/Coronavirus-React/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "15f62d0d8e5c7792665f",
+    "url": "/Coronavirus-React/static/css/main.0879ec8d.chunk.css"
+  },
+  {
+    "revision": "eae77b9b32e0981097c4",
+    "url": "/Coronavirus-React/static/js/2.066feefd.chunk.js"
+  },
+  {
+    "revision": "32aeafb3e2d0a73f27822c8ed8045b88",
+    "url": "/Coronavirus-React/static/js/2.066feefd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f62d0d8e5c7792665f",
+    "url": "/Coronavirus-React/static/js/main.d6d2354f.chunk.js"
+  },
+  {
+    "revision": "c5312b75c51e8b91ea9a",
+    "url": "/Coronavirus-React/static/js/runtime-main.ec682b75.js"
+  }
+]);
